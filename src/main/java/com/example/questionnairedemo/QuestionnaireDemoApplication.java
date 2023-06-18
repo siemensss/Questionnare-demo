f45b6e47bc1201ed89462e67bf8ac7b9f1f3989d
Semyon <sema.perstnev.01@mail.ru>
2023-06-18 20:57:52 +0300

    first commit

---
+++ src/main/java/com/example/questionnairedemo/QuestionnaireDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionnairedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnaireDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/questionnairedemo/QuestionnaireDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionnairedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
